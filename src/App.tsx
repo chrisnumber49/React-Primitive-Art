e0726cc840c332c46a864c9e05d31163f9545832
chrisnumber49 <h28925847@gmail.com>
2024-09-03 20:31:56 +0200

    complete functionalities in AddingShapePanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+import { Stage, Layer, Rect, Circle, RegularPolygon } from 'react-konva';
 import AddingShapePanel from './components/AddingShapePanel'
 
 function App() {
+  const addingNewPattern = (data:object) => {
+    console.warn(data)
+  }
+
   return (
     <div className="main-container">
       <div className="panel-container">
-        <AddingShapePanel/>
+        <AddingShapePanel onAddingNewPattern={addingNewPattern}/>
       </div>
 
       <div className="canvas-container">
         <Stage width={window.innerWidth*0.7} height={window.innerHeight}>
           <Layer>
-            <Rect width={50} height={50} fill="red" />
-            <Circle x={200} y={200} stroke="black" radius={50} />
+            <RegularPolygon x={100} y={100} radius={40} sides={3} fill="green" rotation={0} draggable/>
+
+            <Rect x={300} y={300} width={60} height={60} fill="red" rotation={75} draggable/>
+
+            <Circle x={200} y={200} radius={30} fill="yellow" draggable/>
           </Layer>
         </Stage>
       </div>
--- src/components/AddingShapePanel.tsx
+++ src/components/AddingShapePanel.tsx
@@ -11,22 +11,22 @@ const initialShapeStyles: ShapeStyles = {
     triangleStyle: {
         'width': 0,
         'height': 0,
-        'borderLeft': '50px solid transparent',
-        'borderRight': '50px solid transparent',
-        'borderBottom': '100px solid black',
+        'borderWidth': '0 36px 60px 36px',
+        'borderColor': 'transparent transparent green transparent',
+        'borderStyle': 'solid',
         'rotate': '0deg'
     },
     rectangleStyle: {
-        'width': '75px',
-        'height': '75px',
+        'width': '60px',
+        'height': '60px',
         'borderStyle': 'solid',
         'borderColor': 'red',
         'backgroundColor': 'red',
         'rotate': '0deg'
     },
     circleStyle: {
-        'width': '75px',
-        'height': '75px',
+        'width': '60px',
+        'height': '60px',
         'borderStyle': 'solid',
         'borderColor': 'yellow',
         'backgroundColor': 'yellow',
@@ -34,42 +34,44 @@ const initialShapeStyles: ShapeStyles = {
     }
 }
 
-const reducer = (state: ShapeStyles, action:{shape:string, changeOn:string, value:any}) => {
-    let newParams = {};
+const reducer = (state: ShapeStyles, action:{shape:string, changeOn:string, value:number}) => {
+    let newParams:any = {};
 
     switch (action.shape) {
         case "TRIANGLE":
             newParams = {...state.triangleStyle};
             if(action.changeOn === "ZOOM"){
-
+                newParams.borderWidth = `0 ${0.72*action.value}px ${1.2*action.value}px ${0.72*action.value}px`;
             } else if(action.changeOn === "ROTATION"){
-
+                newParams.rotate = `${action.value}deg`;
             }
 
             return {...state, triangleStyle: newParams}
         case "RECTANGLE":
             newParams = {...state.rectangleStyle};
             if(action.changeOn === "ZOOM"){
-
+                newParams.width = `${1.2*action.value}px`;
+                newParams.height = `${1.2*action.value}px`;
             } else if(action.changeOn === "ROTATION"){
-                
+                newParams.rotate = `${action.value}deg`;
             }
 
             return {...state, rectangleStyle: newParams}
         case "CIRCLE":
             newParams = {...state.circleStyle};
             if(action.changeOn === "ZOOM"){
-
+                newParams.width = `${1.2*action.value}px`;
+                newParams.height = `${1.2*action.value}px`;
             }
 
             return {...state, circleStyle: newParams}
         default:
             return state;
     }
-  };
+};
 
 
-function AddingShapePanel() {
+function AddingShapePanel({onAddingNewPattern}) {
     const [shapeStyles, dispatch] = useReducer(reducer, initialShapeStyles);
     const [currentZoom, setZoom] = useState({triangle: 50,rectangle: 50,circle: 50});
     const [currentRotation, setRotation] = useState({triangle: 0,rectangle: 0});
@@ -88,62 +90,115 @@ function AddingShapePanel() {
         dispatch({ shape, changeOn, value });
     };
 
+    const addNewShapeToCanvas = (shape: "TRIANGLE"| "RECTANGLE"| "CIRCLE") => {
+        let newShape:any = {};
+        if(shape === "TRIANGLE") {
+            newShape = {
+                shape: 'TRIANGLE',
+                radius: 0.8*currentZoom.triangle,
+                rotation: currentRotation.triangle
+            }
+        } else if(shape === "RECTANGLE") {
+            newShape = {
+                shape: 'RECTANGLE',
+                width: 1.2*currentZoom.rectangle,
+                height: 1.2*currentZoom.rectangle,
+                rotation: currentRotation.rectangle
+            }
+        } else if(shape === "CIRCLE") {
+            newShape = {
+                shape: 'CIRCLE',
+                radius: 0.6*currentZoom.circle
+            }
+        }
+
+        onAddingNewPattern(newShape);
+    }
+
     return (
         <div className="panel-layout">
-            <h2 className='text-white'>Control Panel</h2>
-            <div className="panel-item">
-                <div style={shapeStyles.triangleStyle}/>
+            <h2 className='m-2'>Control Panel</h2>
 
-                <input
-                    type="range"
-                    id="triangleZoom"
-                    min="1"
-                    max="100"
-                    value={currentZoom.triangle}
-                    onChange={(event)=> {handleShapeChange("TRIANGLE", "ZOOM", Number(event.target.value))}}
-                ></input>
-                <input
-                    type="range"
-                    id="triangleRotate"
-                    min="0"
-                    max="359"
-                    value={currentRotation.triangle}
-                    onChange={(event)=> {handleShapeChange("TRIANGLE", "ROTATION", Number(event.target.value))}}
-                ></input>
+            <div className="panel-item">
+                <div className="shape-container">
+                    <div style={shapeStyles.triangleStyle}/>
+                </div>
+
+                <div className="input-group my-2">
+                    <label className='mx-2' htmlFor="triangleZoom">Zooming</label>
+                    <input
+                        type="range"
+                        id="triangleZoom"
+                        min="10"
+                        max="100"
+                        value={currentZoom.triangle}
+                        onChange={(event)=> {handleShapeChange("TRIANGLE", "ZOOM", Number(event.target.value))}}
+                    ></input>
+                </div>
+                <div className="input-group my-2">
+                    <label className='mx-2' htmlFor="triangleRotate">Rotation</label>
+                    <input
+                        type="range"
+                        id="triangleRotate"
+                        min="0"
+                        max="359"
+                        value={currentRotation.triangle}
+                        onChange={(event)=> {handleShapeChange("TRIANGLE", "ROTATION", Number(event.target.value))}}
+                    ></input>
+                </div>
+
+                <button className="add-btn btn btn-primary" onClick={() => {addNewShapeToCanvas("TRIANGLE")}}>Add</button>
             </div>
             
             <div className="panel-item">
-                <div style={shapeStyles.rectangleStyle}/>
+                <div className="shape-container">
+                    <div style={shapeStyles.rectangleStyle}/>
+                </div>
 
-                <input
-                    type="range"
-                    id="rectangleZoom"
-                    min="1"
-                    max="100"
-                    value={currentZoom.rectangle}
-                    onChange={(event)=> {handleShapeChange("RECTANGLE", "ZOOM", Number(event.target.value))}}
-                ></input>
-                <input
-                    type="range"
-                    id="rectangleRotate"
-                    min="0"
-                    max="359"
-                    value={currentRotation.rectangle}
-                    onChange={(event)=> {handleShapeChange("RECTANGLE", "ROTATION", Number(event.target.value))}}
-                ></input>
+                <div className="input-group my-2">
+                    <label className='mx-2' htmlFor="rectangleZoom">Zooming</label>
+                    <input
+                        type="range"
+                        id="rectangleZoom"
+                        min="10"
+                        max="100"
+                        value={currentZoom.rectangle}
+                        onChange={(event)=> {handleShapeChange("RECTANGLE", "ZOOM", Number(event.target.value))}}
+                    ></input>
+                </div>
+                <div className="input-group my-2">
+                    <label className='mx-2' htmlFor="rectangleRotate">Rotation</label>
+                    <input
+                        type="range"
+                        id="rectangleRotate"
+                        min="0"
+                        max="359"
+                        value={currentRotation.rectangle}
+                        onChange={(event)=> {handleShapeChange("RECTANGLE", "ROTATION", Number(event.target.value))}}
+                    ></input>
+                </div>
+
+                <button className="add-btn btn btn-primary" onClick={() => {addNewShapeToCanvas("RECTANGLE")}}>Add</button>
             </div>
 
             <div className="panel-item">
-                <div style={shapeStyles.circleStyle}/>
+                <div className="shape-container">
+                    <div style={shapeStyles.circleStyle}/>
+                </div>
 
-                <input
-                    type="range"
-                    id="circleZoom"
-                    min="1"
-                    max="100"
-                    value={currentZoom.circle}
-                    onChange={(event)=> {handleShapeChange("CIRCLE", "ZOOM", Number(event.target.value))}}
-                ></input>
+                <div className="input-group my-2">
+                    <label className='mx-2' htmlFor="circleZoom">Zooming</label>
+                    <input
+                        type="range"
+                        id="circleZoom"
+                        min="10"
+                        max="100"
+                        value={currentZoom.circle}
+                        onChange={(event)=> {handleShapeChange("CIRCLE", "ZOOM", Number(event.target.value))}}
+                    ></input>
+                </div>
+
+                <button className="add-btn btn btn-primary" onClick={() => {addNewShapeToCanvas("CIRCLE")}}>Add</button>
             </div>
         </div>
     )
